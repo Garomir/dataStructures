67fccc05796615bb02d7fd902f4cd530f3ee7e3f
Ramil Safuanov <ramilfin@mail.ru>
2021-12-05 10:48:53 +0300

    added linkedList

--- src/com/ramich/dataStructures/MyLinkedList/App.java
+++ src/com/ramich/dataStructures/MyLinkedList/App.java
@@ -2,6 +2,14 @@ package com.ramich.dataStructures.MyLinkedList;
 
 public class App {
     public static void main(String[] args) {
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
 
+        myLinkedList.add(10);
+        myLinkedList.add(20);
+        myLinkedList.add(30);
+        myLinkedList.display();
+
+        myLinkedList.addToHead(55);
+        myLinkedList.display();
     }
 }
--- src/com/ramich/dataStructures/MyLinkedList/MyLinkedList.java
+++ src/com/ramich/dataStructures/MyLinkedList/MyLinkedList.java
@@ -1,4 +1,43 @@
 package com.ramich.dataStructures.MyLinkedList;
 
-public class MyLinkedList {
+public class MyLinkedList<T> {
+
+    private Node head;
+
+    public void add(T item){
+        Node node = new Node();
+        node.data = item;
+        node.next = null;
+
+        if (head == null){
+            head = node;
+        } else {
+            Node current = head;
+            while (current.next != null){
+                current = current.next;
+            }
+            current.next = node;
+        }
+    }
+
+    public void addToHead(T item){
+        Node node = new Node();
+        node.data = item;
+        node.next = head;
+        head = node;
+    }
+
+    public void display(){
+        Node current = head;
+        while (current != null){
+            System.out.print(" " + current.data);
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+    }
 }
